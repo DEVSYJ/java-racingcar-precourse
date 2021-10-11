3feae5143821f59b41196ea2ffae4b51e5ff17fb
JSY <tjdudd2@naver.com>
2021-10-12 01:49:10 +0900

    test(ParticipantCarsTest): add moveParticipantCarsSuccessTest

--- src/test/java/racinggame/model/ParticipantCarsTest.java
+++ src/test/java/racinggame/model/ParticipantCarsTest.java
@@ -10,9 +10,9 @@ import exception.InputException;
 class ParticipantCarsTest {
 	private static final String[] participantCarsNames
 		= {"pobi,crong,honux", "pobi", ",crong,", ",,honux", "pobi,,", "a b, c d"};
-
 	private static final String[] missMatchParticipantCarsNames
 		= {"pobi123,crong,honux", "pobi123", ",crong123,", ",,honux123", "pobi123,,", "a b, c d123"};
+	private static final String participantCarsName = "pobi,crong,honux";
 
 	@Test
 	void createParticipantCarsSuccessTest() {
@@ -32,6 +32,17 @@ class ParticipantCarsTest {
 			assertThrows(InputException.class, () -> {
 				new ParticipantCars(participantCarsNamesString);
 			});
+		}
+	}
+
+	@Test
+	void moveParticipantCarsSuccessTest() {
+		ParticipantCars participantCars = new ParticipantCars(participantCarsName);
+		participantCars.moveEachCar();
+
+		for (int i = 0; i < 3; i++) {
+			assertThat(participantCars.getParticipants().get(i).getMoveCount().getValue())
+				.isNotNegative();
 		}
 	}
 }
